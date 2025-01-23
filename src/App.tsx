9d88fe15ff6f9fefdef878a7c2e33d0ff0761db9
iequivocality <xanthiandere@gmail.com>
2025-01-23 11:28:19 +0800

    Add biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <main className="bg-blue-950 text-white h-screen w-screen">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </main>
-  )
+	return (
+		<main className="bg-blue-950 text-white h-screen w-screen">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</main>
+	);
 }
 
-export default App
+export default App;
