5ed6cbbac44802a478a77a35018bdf9a930ce969
iequivocality <xanthiandere@gmail.com>
2025-01-23 10:43:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-react-tenpureeto
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules. Tailwind CSS is also added to the project.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="bg-blue-950 text-white h-screen w-screen">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
+
+export default App
